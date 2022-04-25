44934df478e7bbd8a22050f66b870c02aeee9c7e
vmcenteruk <vmcenteruk@gmail.com>
2022-04-25 12:55:34 +0100

    algumas correções

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /*
-ainda é preciso limitar a quantidade de caracteres 
-em parte inteira do número introduzido. Depois de 16 
-caracteres pode dar erro (os caracteres seguintes apresentam-se como 0).
+Ainda é preciso limitar a quantidade de caracteres em parte inteira dos números introduzidos. 
+Depois de 16 caracteres pode dar erro. 
+Os caracteres seguintes apresentam-se como 0.
 */
 
 /* 
-o calculador tem a seguinte lógica:
-as botóes "+ - / *" desempenham a função "=" em alguns casos:
-quando chamados logo depois de um deles sem indtroduzir algum número.
-Exemplo: 
+A calculadora tem a seguinte lógica: as botões + - / * desempenham 
+a função "=" em alguns casos: 
+quando chamados imediatamente depois de um deles sem indtroduzir algum número. 
+Exemplos:
 6-+ => 6+ (ou seja: 6-0=6 => 6+)
 6*+ => 0+ (ou seja: 6*0=0 => 0+)
 6/+ => not a number (ou seja: 6/0=not a number)
